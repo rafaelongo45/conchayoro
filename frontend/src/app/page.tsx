95f0c7e70fcad30e907c30f12733cca6a29a1861
Rafael Longo <rafaelongo45@gmail.com>
2024-09-13 00:08:13 +0000

    merging with main

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export default function Home() {
 return (
 <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -9,3 +10,21 @@ return (
 </main>
 )
 }
+=======
+import { blackFridayFlag } from './flag.tsx';
+
+export default async function Page() {
+  const black_friday = await blackFridayFlag();
+
+  return (
+    <html>
+      <body>
+        <div>
+          <h1> Welcome to the ConchaYOro App - deploy v3a</h1>
+          { black_friday ? <button> Promo </button> : <button> Normal </button> } 
+        </div>
+      </body>
+    </html>
+  )
+};
+>>>>>>> a7c8c44 (includes feature flag)
