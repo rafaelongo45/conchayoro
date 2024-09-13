c6c26af2044badcdaa4d769f1ed2525affd6a4fa
Rafael Longo <rafaelongo45@gmail.com>
2024-09-13 00:11:05 +0000

    fixing merge flags

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-export default function Home() {
-return (
-<main className="flex min-h-screen flex-col items-center justify-between p-24">
-  <div className="z-10 max-w-5xl w-full items-center justify-between font-mono
-  text-sm lg:flex">
-    <h1> Welcome to the ConchaYOro App - deploy v2</h1>
-    <h2> This is a really cool website! </h2>
-  </div>
-</main>
-)
-}
-=======
 import { blackFridayFlag } from './flag.tsx';
 
 export default async function Page() {
@@ -27,4 +14,3 @@ export default async function Page() {
     </html>
   )
 };
->>>>>>> a7c8c44 (includes feature flag)
